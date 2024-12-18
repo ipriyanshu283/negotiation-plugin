0f8f68c6636f0f5761402abc8032341f9153e9f3
Priyanshu Aggarwal <113134715+ipriyanshu283@users.noreply.github.com>
2024-12-18 15:50:06 +0530

    Update negotiationService.js

--- negotiation-frontend/src/services/negotiationService.js
+++ negotiation-frontend/src/services/negotiationService.js
@@ -1,6 +1,6 @@
 // negotiationService.js
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/negotiations";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Create a new offer
 export const createOffer = async (buyerEmail, sellerEmail, offerAmount) => {
