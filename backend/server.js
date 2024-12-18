d34bf2ec1e3aa8a09f5d49bfc37d484d4319eda0
Priyanshu Aggarwal <113134715+ipriyanshu283@users.noreply.github.com>
2024-12-18 21:15:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,31 +10,31 @@ const app = express();
 
 // Connect to MongoDB
 connectDB();
-// dja?
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://negotiation-plugin-frontend.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
 
-  if (req.method === "OPTIONS") {
-    return res.status(200).end(); // Send 200 OK for preflight
-  }
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://negotiation-plugin-frontend.vercel.app");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.header("Access-Control-Allow-Credentials", "true");
 
-  next();
-});
+//   if (req.method === "OPTIONS") {
+//     return res.status(200).end(); // Send 200 OK for preflight
+//   }
+
+//   next();
+// });
 
 
 
 // app.use(cors());
 // Middleware
-// app.use(cors({
-//   origin: "https://negotiation-plugin-frontend.vercel.app",
-//   methods: ["POST","GET"],
-//   credentials: true
-// }));
+app.use(cors({
+  origin: "https://negotiation-plugin-frontend.vercel.app",
+  methods: ["POST","GET"],
+  credentials: true
+}));
 app.use(bodyParser.json());
 
 // Routes
