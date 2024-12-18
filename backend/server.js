ed056a9b17f26e44ef368d852ba78a779ad25fdb
Priyanshu Aggarwal <113134715+ipriyanshu283@users.noreply.github.com>
2024-12-18 20:55:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,25 @@ const app = express();
 
 // Connect to MongoDB
 connectDB();
-app.use(cors());
+
+
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://negotiation-plugin-frontend.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end(); // Send 200 OK for preflight
+  }
+
+  next();
+});
+
+
+
+// app.use(cors());
 // Middleware
 // app.use(cors({
 //   origin: "https://negotiation-plugin-frontend.vercel.app",
